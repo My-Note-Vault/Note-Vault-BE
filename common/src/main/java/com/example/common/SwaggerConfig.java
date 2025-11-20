39125e5c065f9a9c5e1c05211558e5ab2ccdad26
kwanse <khan67781397@gmail.com>
2025-11-20 21:19:02 +0900

    [doc] Swagger Configuration 추가

---
+++ common/src/main/java/com/example/common/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.common;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components());
+    }
+
+}
