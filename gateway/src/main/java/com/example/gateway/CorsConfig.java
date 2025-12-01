ecde71688140d2a4441f6f2c81575eb9042d9408
kwanse <khan67781397@gmail.com>
2025-12-01 20:06:23 +0900

    [chore] CORS 설정 적용

---
+++ gateway/src/main/java/com/example/gateway/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.example.gateway;
+
+import com.example.platformservice.auth.WebConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${client.host}")
+    private String host;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebConfig() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(host)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
