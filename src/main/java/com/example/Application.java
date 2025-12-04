671f93b0becf38f123327bc36d334f6acb522978
kwanse <khan67781397@gmail.com>
2025-12-04 19:56:39 +0900

    [chore] Application 관련 멀티모듈 세팅

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        run(Application.class, args);
+    }
+
+}
