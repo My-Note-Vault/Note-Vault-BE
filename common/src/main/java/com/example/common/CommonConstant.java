90e0a906bd686e2509a412be962b0aa0aa202120
kwanse <khan67781397@gmail.com>
2026-02-23 19:07:22 +0900

    [chore] 공통 상수 클래스 추가

---
+++ common/src/main/java/com/example/common/CommonConstant.java
@@ -0,0 +1,8 @@
+package com.example.common;
+
+public final class CommonConstant {
+
+    private CommonConstant() {}
+
+    public static final String AUTHORIZED_MEMBER_ID = "Authorized-Member-Id";
+}
