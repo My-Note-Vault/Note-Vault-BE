022b9bc7ccd6401de5febd2d61e4a6ea16f58edb
kwanse <khan67781397@gmail.com>
2026-03-23 20:46:56 +0900

    [fix] @Configuration 추가

--- common/src/main/java/com/example/common/WebCorsConfig.java
+++ common/src/main/java/com/example/common/WebCorsConfig.java
@@ -1,8 +1,10 @@
 package com.example.common;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebCorsConfig implements WebMvcConfigurer {
 
     @Override
