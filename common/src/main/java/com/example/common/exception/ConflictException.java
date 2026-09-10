154cbed9997376154e78ed28575d197e676db516
kwanse <khan67781397@gmail.com>
2026-09-10 21:03:32 +0900

    [fix] Exception 추가

---
+++ common/src/main/java/com/example/common/exception/ConflictException.java
@@ -0,0 +1,8 @@
+package com.example.common.exception;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(final String message) {
+        super(message);
+    }
+}
